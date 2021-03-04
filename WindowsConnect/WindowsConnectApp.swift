4f7824d05376384368dd11f6c2f426178b276b35
Kenny <kenny.langston@gmail.com>
2021-03-04 13:16:58 -0700

    Initial Commit

---
+++ WindowsConnect/WindowsConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowsConnectApp.swift
+//  WindowsConnect
+//
+//  Created by Kenny Langston on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct WindowsConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
